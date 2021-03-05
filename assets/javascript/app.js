3eefdae1d5951f99bfcbf9e7b3477f833907a81b
Max Stevenson <maxstevenson@msn.com>
2021-03-04 19:21:45 -0600

    - Good progress on logic for selecting firing settings.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,17 @@
-const navigateOptions = function () {
-  let startElement = document.querySelector('.settings-container__top-section--active').parentElement;
+let currentIndex = 0;
 
+const navigateOptions = function () {
   document.onkeydown = checkKey;
 
   function checkKey(e) {
-
     switch (e.key) {
       case "ArrowLeft":
         // Left pressed
         break;
       case "ArrowRight":
         // Right pressed
-        startElement.nextElementSibling.classList.toggle('settings-container__top-section--active');
+        currentIndex++;
+        renderActiveSetting(currentIndex);
         break;
       case "ArrowUp":
         // Up pressed
@@ -21,6 +21,33 @@ const navigateOptions = function () {
         break;
     }
   }
-}
+};
 
 navigateOptions();
+
+const renderActiveSetting = (index) => {
+  const outerSettings = document.querySelectorAll(
+    ".console__settings-container"
+  );
+  let currentSettingElement = outerSettings[index];
+  let previousSettingElement = document.querySelector(
+    ".settings-container__top-section--active"
+  );
+  if (previousSettingElement) {
+    previousSettingElement.classList.toggle(
+      "settings-container__top-section--active"
+    );
+  }
+  currentSettingElement.firstElementChild.classList.toggle(
+    "settings-container__top-section--active"
+  );
+};
+
+renderActiveSetting(currentIndex);
+/*
+  make checkKey generic to navigate outer option and inner option
+  have a move outer option
+  keep track of outer with isOuterSelection
+  enter key moves to internal selection function, second enter key moves back out to outer + esc key?
+
+*/
