746e5789ead983c4640638476a26df8b4d4d6558
Max Stevenson <maxstevenson@msn.com>
2021-03-07 19:18:30 -0600

    - Big refactor of existing code base. - Moved functions inside of handleNavigation func. - Extracted arrow up and down logic into own func.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,3 @@
-const renderActiveSetting = (index) => {
-  const outerSettings = document.querySelectorAll(".console__settings-container");
-  let currentSettingElement = outerSettings[index];
-  let previousSettingElement = document.querySelector(".settings-container__top-section--active");
-  if (previousSettingElement) {
-    previousSettingElement.classList.toggle("settings-container__top-section--active");
-  }
-  currentSettingElement.firstElementChild.classList.toggle(
-    "settings-container__top-section--active"
-  );
-};
-
 const handleNavigation = function () {
   let currentOuterIndex = 0;
   const maxOuterIndex = document.querySelectorAll(".console__settings-container").length - 1;
@@ -18,10 +6,58 @@ const handleNavigation = function () {
   let maxInnerIndex;
   let previousOption;
   let currentInnerIndex = 0;
+
+  const renderActiveSetting = (index) => {
+    const outerSettings = document.querySelectorAll(".console__settings-container");
+    let currentSettingElement = outerSettings[index];
+    let previousSettingElement = document.querySelector(".settings-container__top-section--active");
+    if (previousSettingElement) {
+      previousSettingElement.classList.toggle("settings-container__top-section--active");
+    }
+    currentSettingElement.firstElementChild.classList.toggle(
+      "settings-container__top-section--active"
+    );
+  };
   renderActiveSetting(currentOuterIndex);
 
-  document.onkeydown = checkKey;
+  const renderActiveInner = (currentInnerIndex) => {
+    innerOptions = currentSettingElement.parentElement.lastElementChild.children;
+    previousOption = currentSettingElement.parentElement.lastElementChild.querySelector(
+      ".settings-container__option--active"
+    );
+    previousOption.classList.toggle("settings-container__option--active");
+    innerOptions[currentInnerIndex].classList.toggle("settings-container__option--active");
+  };
 
+  const selectSettingBlock = function (index) {
+    const outerSettings = document.querySelectorAll(".console__settings-container");
+    let currentSettingElement = outerSettings[index];
+    currentSettingElement.firstElementChild.classList.toggle(
+      "settings-container__top-section--active"
+    );
+    currentSettingElement.lastElementChild.firstElementChild.classList.toggle(
+      "settings-container__option--active"
+    );
+  };
+
+  const resetSettings = () => {
+    selectedSettings = document.querySelectorAll(".settings-container__option--active");
+    if (selectedSettings) {
+      for (let i = 0, j = selectedSettings.length; i < j; i++) {
+        selectedSettings[i].classList.toggle("settings-container__option--active");
+      }
+    }
+  
+    let currentActive = document.querySelector(".settings-container__top-section--active");
+    if (currentActive) {
+      document
+        .querySelector(".settings-container__top-section--active")
+        .classList.toggle("settings-container__top-section--active");
+    }
+    renderActiveSetting(0);
+  };
+
+  document.onkeydown = checkKey;
   function checkKey(e) {
     let innerOptions;
 
@@ -48,35 +84,20 @@ const handleNavigation = function () {
       case "ArrowUp":
         // Up pressed
         if (!outerSelectionActive) {
-          console.log(currentInnerIndex);
           currentInnerIndex--;
-          console.log(currentInnerIndex);
-
           if (currentInnerIndex < 0) {
             currentInnerIndex = 0;
           }
-          innerOptions = currentSettingElement.parentElement.lastElementChild.children;
-          previousOption = currentSettingElement.parentElement.lastElementChild.querySelector(
-            ".settings-container__option--active"
-          );
-          previousOption.classList.toggle("settings-container__option--active");
-          innerOptions[currentInnerIndex].classList.toggle("settings-container__option--active");
+          renderActiveInner(currentInnerIndex);
         }
         break;
       case "ArrowDown": // Down pressed
         if (!outerSelectionActive) {
-          console.log(currentInnerIndex);
           currentInnerIndex++;
-          console.log(currentInnerIndex);
           if (currentInnerIndex > maxInnerIndex) {
             currentInnerIndex = maxInnerIndex;
           }
-          innerOptions = currentSettingElement.parentElement.lastElementChild.children;
-          previousOption = currentSettingElement.parentElement.lastElementChild.querySelector(
-            ".settings-container__option--active"
-          );
-          previousOption.classList.toggle("settings-container__option--active");
-          innerOptions[currentInnerIndex].classList.toggle("settings-container__option--active");
+          renderActiveInner(currentInnerIndex);
         }
 
         break;
@@ -105,39 +126,3 @@ const handleNavigation = function () {
 };
 
 handleNavigation();
-
-const selectSettingBlock = function (index) {
-  const outerSettings = document.querySelectorAll(".console__settings-container");
-  let currentSettingElement = outerSettings[index];
-  currentSettingElement.firstElementChild.classList.toggle(
-    "settings-container__top-section--active"
-  );
-  currentSettingElement.lastElementChild.firstElementChild.classList.toggle(
-    "settings-container__option--active"
-  );
-};
-
-const resetSettings = () => {
-  selectedSettings = document.querySelectorAll(".settings-container__option--active");
-  if (selectedSettings) {
-    for (let i = 0, j = selectedSettings.length; i < j; i++) {
-      selectedSettings[i].classList.toggle("settings-container__option--active");
-    }
-  }
-
-  let currentActive = document.querySelector(".settings-container__top-section--active");
-  if (currentActive) {
-    document
-      .querySelector(".settings-container__top-section--active")
-      .classList.toggle("settings-container__top-section--active");
-  }
-  renderActiveSetting(0);
-};
-
-/*
-  make checkKey generic to navigate outer option and inner option
-  have a move outer option
-  keep track of outer with isOuterSelection
-  enter key moves to internal selection function, second enter key moves back out to outer + esc key?
-
-*/
