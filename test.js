1943ca246bca7a417c5d9a9afc14212f7738dfa7
Max Stevenson <maxstevenson@msn.com>
2021-03-16 18:56:23 -0500

    - Experimenting with temp gauge logic. - Fixed temp gauge to be right side up.

--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 const handleFiring = () => {
   const statusBar = document.querySelector(".status-bar__inner");
+  const statusBarParent = statusBar.parentElement;
   document.addEventListener("keydown", function (e) {
     if (e.keyCode === 32) {
-      statusBar.style.height += "5%";
+      const previousHeightPercentage = Number(statusBar.offsetHeight) / Number(statusBarParent.offsetHeight) * 100;
+      console.log(previousHeightPercentage);
+      statusBar.style.height = `${previousHeightPercentage + 2}%`;
     }
   });
 };
