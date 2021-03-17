dd1bbd4f40832ab58b192ac400cd707d53c3451c
Max Stevenson <maxstevenson@msn.com>
2021-03-16 19:21:39 -0500

    - Good progress made on temp gauge.

--- test.js
+++ test.js
@@ -3,11 +3,22 @@ const handleFiring = () => {
   const statusBarParent = statusBar.parentElement;
   document.addEventListener("keydown", function (e) {
     if (e.keyCode === 32) {
-      const previousHeightPercentage = Number(statusBar.offsetHeight) / Number(statusBarParent.offsetHeight) * 100;
-      console.log(previousHeightPercentage);
-      statusBar.style.height = `${previousHeightPercentage + 2}%`;
+      const previousHeightPercentage = Math.floor(
+        (Number(statusBar.offsetHeight) / Number(statusBarParent.offsetHeight)) * 100
+      );
+      statusBar.style.height = `${
+        previousHeightPercentage + 3 > 100 ? 100 : previousHeightPercentage + 3
+      }%`;
     }
   });
+  setInterval(() => {
+    if (statusBar.offsetHeight !== 0) {
+      console.log("meow");
+      const previousHeightPercentage = Math.floor(
+        (Number(statusBar.offsetHeight) / Number(statusBarParent.offsetHeight)) * 100
+      );
+      statusBar.style.height = `${previousHeightPercentage - 3}%`;
+    }
+  }, 500);
 };
-
 handleFiring();
